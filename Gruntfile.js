d6ce33b3de53fe336a52fbb9e78d05706ffc15d2
Sarah  Strange <sarahstrange@sarahs-mbp.attlocal.net>
2018-09-19 00:13:27 -0400

    basic structure

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+const sass = require('node-sass');
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sass: {
+	options: {
+		implementation:sass
+},
+      dist: {
+        files: {
+          'public/stylesheets/style.css': 'sass/style.scss'
+        }
+      }
+    },
+    watch: {
+      source: {
+        files: ['sass/**/*.scss', 'views/**/*.jade'],
+        tasks: ['sass'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.registerTask('default', ['sass']);
+};
